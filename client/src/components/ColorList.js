7d03739557d37b14f10fb839e1286878c70b52e8
Asher Kobin <Asher@Ashers-MBP.fios-router.home>
2020-01-31 16:25:56 -0500

    Stretch

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -9,22 +9,44 @@ const initialColor = {
 const ColorList = ({ colors, updateColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
+  const [adding, setAdding] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+  const [colorToAdd, setColorToAdd] = useState(initialColor);
 
   const editColor = color => {
     setEditing(true);
+    setAdding(false);
     setColorToEdit(color);
+  };
+
+  const addColor = color => {
+    setAdding(true);
+    setEditing(false);
   };
 
   const saveEdit = e => {
     e.preventDefault();
+    setEditing(false);
     axiosWithAuth()
       .put("http://localhost:5000/api/colors/" + colorToEdit.id, colorToEdit)
       .then(res => updateColors([...colors.filter(c => c.id !== colorToEdit.id), colorToEdit]))
       .catch(err => console.log(err));
+    setColorToEdit(initialColor);
+  };
+
+  const saveAdd = e => {
+    e.preventDefault();
+    setAdding(false);
+    axiosWithAuth()
+      .post("http://localhost:5000/api/colors", colorToAdd)
+      .then(res => updateColors(res.data))
+      .catch(err => console.log(err));
+    setColorToAdd(initialColor);
   };
 
   const deleteColor = color => {
+    setColorToAdd(initialColor);
+    setColorToEdit(initialColor);
     axiosWithAuth()
       .delete("http://localhost:5000/api/colors/" + color.id)
       .then(res => updateColors(colors.filter(c => c.id !== color.id)))
@@ -33,7 +55,7 @@ const ColorList = ({ colors, updateColors }) => {
 
   return (
     <div className="colors-wrap">
-      <p>colors</p>
+      <p>colors <button onClick={addColor}>Add</button></p>
       <ul>
         {colors.map(color => (
           <li key={color.color} onClick={() => editColor(color)}>
@@ -80,12 +102,40 @@ const ColorList = ({ colors, updateColors }) => {
           </label>
           <div className="button-row">
             <button type="submit">save</button>
-            <button onClick={() => setEditing(false)}>cancel</button>
+            <button onClick={() => { setEditing(false); setColorToEdit(initialColor); }}>cancel</button>
           </div>
         </form>
       )}
-      <div className="spacer" />
-      {/* stretch - build another form here to add a color */}
+      {adding && (
+        <form onSubmit={saveAdd}>
+        <legend>add color</legend>
+        <label>
+          color name:
+          <input
+            onChange={e =>
+              setColorToAdd({ ...colorToAdd, color: e.target.value })
+            }
+            value={colorToAdd.color}
+          />
+        </label>
+        <label>
+          hex code:
+          <input
+            onChange={e =>
+              setColorToAdd({
+                ...colorToAdd,
+                code: { hex: e.target.value }
+              })
+            }
+            value={colorToAdd.code.hex}
+          />
+        </label>
+        <div className="button-row">
+          <button type="submit">save</button>
+          <button onClick={() => { setAdding(false); setColorToAdd(initialColor); }}>cancel</button>
+        </div>
+      </form>
+      )}
     </div>
   );
 };
